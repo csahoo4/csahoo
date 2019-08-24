f50afc8778238c3ebc9c4ef47dba48d1706915b5
Chiranjeeb Sahoo <chiranjeebsahoo4@gmail.com>
2019-08-25 03:55:04 +0530

    updated profile page

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function () {
+  let btn = $("#btn");
+  let textAboutMe = $("#text-about-me");
+  btn.click(() => {
+    textAboutMe.empty();
+    textAboutMe.append(`Hello! My name is Chiranjeeb Sahoo. Well what to tell about myself, I am a true Cricket lover and love to 
+    code as well. I love to make new projects as they help you to learn the concepts and along with that when 
+    you implement something practically then you really learn a lot. I have always believed that "self-learning 
+    is the best learning" and making projects is the best way to learn on your own. Watching cricket and doing 
+    coding is my favorite hobby, I just love them. Apart from cricket, I like badminton too but the craze for cricket 
+    is obviously more. I am a big Cricket fan. I have been watching cricket since I was 7-8 year old (can't remember 
+    exact age). I just love watching Cricket, I do not know what happens to me whenever Indian Cricket team plays, at
+     that moment I am so busy observing each and every run that is scored on the field that I forget everything else 
+     and sometimes I even shout at players like "yeh kaise out ho gaya yaar" whenever any Indian player gets out 
+     playing bad shot XD. Having said that I must mention the name, the one and only <a class="white-link" href="https://en.wikipedia.org/wiki/MS_Dhoni" target="_blank">MSD</a>. He has been my hero since I 
+     started watching Cricket. He is one of the best keeper-batsman the world has ever seen and I think you will agree 
+     with me :). Now let's talk about food. In food, I like Veg Chowmien, French Fries, Aaloo Ka Parantha, Shahi Paneer, 
+     Chole Kulche, Chole Bhature, White Sauce Pasta, Red Sauce Pasta, Butter Chicken, Tandoori Chicken and lots of more 
+     stuff, obviously I can't name all of them. Mind you I am not at all foodie (see my picture ;)). As far as the coding 
+     part is concerned, details about that are given in the profile tab. I have a craze for coding for sure. 
+     It definitely excites me and what it does is that it increases your ability to think logically. Coding has 
+     definitely helped me and I hope it will help me in my career ahead as well when I start searching for the job. 
+     Currently, I am studying in ADGITM and want to study as well as I can and become a successful Engineer in the future.`)
+  });
+})
